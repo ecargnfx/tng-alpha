7bc497a785b18b0f481816a856e89570c7825f39
Jay Deuskar <coffeejay@me.com>
2013-09-22 11:36:32 -0400

    Quick fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def generate
     alchemy = AlchemyAPI.new("2094dd01fd7cbceb7e1bb916840e40e81f25d16f")
     results = alchemy.textGetRankedKeywords(params[:q])
-    marketing = ['marketing', 'web page', 'webpage', 'info', 'information', 'informative', 'business', 'advertise', 'advertisement', 'advertis']
+    marketing = ['marketing', 'web page', 'webpage', 'info', 'information', 'informative', 'business', 'advertise', 'advertisement', 'advertis', 'event', 'promot']
     blog = ['blog', 'post', 'blog post', 'journal', 'timeline', 'diary', 'list']
     contact = ['contact', 'message', 'about', 'email']
     landing = ['landing', 'splash', 'customer discovery', 'teaser']
